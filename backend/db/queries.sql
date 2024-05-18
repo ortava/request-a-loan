6fd28d96fb4aedbabc4bc4d14542f719fd58e2e3
ortava <RyanTommyTavares@gmail.com>
2024-05-17 19:22:42 -0700

    Add sqlite database SQL commands to create the applications table are included in queries.sql

---
+++ backend/db/queries.sql
@@ -0,0 +1,20 @@
+-- SQLite
+
+-- Get everything from applications table
+SELECT * FROM applications;
+
+-- Create applications table
+CREATE TABLE applications(
+	application_id	INTEGER,
+	user_id	INTEGER NOT NULL DEFAULT -1,
+	name    TEXT NOT NULL DEFAULT ' ' CHECK(LENGTH("name") <= 100),
+	requested_amount	REAL NOT NULL DEFAULT 0.00 CHECK("requested_amount" >= 0),
+	approval_status	TEXT NOT NULL DEFAULT 'Under Review' CHECK("approval_status" IN ('Approved', 'Declined', 'Under Review')),
+	PRIMARY KEY(application_id)
+)
+
+-- Delete everything in applications table
+DELETE FROM applications;
+
+-- Drop the applications table (so you can recreate it)
+DROP TABLE applications;
